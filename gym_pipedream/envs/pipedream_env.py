9d4ba743ee426cd03996bcb8f9d95f3b0306d340
Jelso13 <jameselson19@gmail.com>
2022-06-09 22:48:41 +0100

    Moved the grid/board class to its own class in grid_elements to make the code cleaner and easier to manage

--- gym_pipedream/envs/pipedream_env.py
+++ gym_pipedream/envs/pipedream_env.py
@@ -15,20 +15,19 @@ class PipeDreamEnv(gym.Env):
     def __init__(self, render_mode = "ascii", width = BOARD_WIDTH, height = BOARD_HEIGHT):
         assert render_mode in self.metadata["render_modes"]
 
-        self.width = width  # width of the playing area
-        self.height = height  # height of the playing area
-        self.window_size = 512  # The size of the PyGame window
-        #self.board = [[(0, 'none') for i in range(BOARD_WIDTH)] for j in range(BOARD_HEIGHT)]
-        self.board = [Floor()] * self.width * self.height
+        self.render_mode = render_mode
+
+        self.board = Board(width, height)
         self.next_tiles = [None] * TILE_QUEUE_LEN
         self.current_tile = None
 
         # observation space consists of grid representing every square
-        self.observation_space = spaces.Box(low=OBS_LOW, high=OBS_HIGH, shape=(self.width, self.height), dtype=np.int8)
-
+        self.observation_space = spaces.Box(low=OBS_LOW, high=OBS_HIGH, shape=(width, height), dtype=np.int8)
         # one action for each position on the grid
-        self.action_space = spaces.MultiDiscrete([self.width, self.height])
+        self.action_space = spaces.MultiDiscrete([width, height])
         
+        self.window_size = 512  # The size of the PyGame window
+
         # handle the pygame render if render mode is human
         if render_mode == "human":
             import pygame  # import here to avoid pygame dependency
@@ -43,73 +42,59 @@ class PipeDreamEnv(gym.Env):
         #super().reset(seed=seed) # seed self.np_random
 
         # Reset the board
-        self.board = [Floor()] * self.width * self.height
+        self.board.reset_board()
 
         # init tap location
-        self.init_tap()
+        self.board.init_tap()
 
         # init walls if any
 
         # init list of next tiles
-        self.next_tiles = [random.choice(PLAYING_TILES) for i in range(TILE_QUEUE_LEN)]
+        self.next_tiles = [random.choice(PLAYING_TILES)() for i in range(TILE_QUEUE_LEN)]
         self.current_tile = self.next_tiles[-1]
 
         return self._get_observation()
 
-    def render(self):
+    def step(self, action):
+        """
+        This method takes an action in the current state by 
+        placing/replacing a block on the grid then returns the 
+        new state, the reward obtained and whether the next 
+        state is terminal.
+
+        actions are enumerated from 0 to board_width * board_height - 1 corresponding to grid positions
+        """
+        assert action >= 0 and action <= self.width * self.height -1 
+
+        """
+        if possible action:
+            modify the board
+            determine the reward given a get_reward method ** when new pipe fills then give reward next action
+            determine if the environment is done (the pipe has failed)
+            get the observation of the new state
+            return all
+        """
+
+
         raise NotImplementedError
 
-    def step(self):
-        raise NotImplementedError
+    def render(self):
+        if self.render_mode == "ascii":
+            print(self.board)
+        else:
+            raise NotImplementedError
 
     def _get_observation(self):
-        return {"board":[tile.get_encoding() for tile in self.board], "current_tile":self.current_tile.get_encoding()}
+        return {"board":[tile.get_encoding() for tile in self.board.get_tiles()], "current_tile": self.current_tile.get_encoding()}
 
-    def init_tap(self):
-        tap_location = list(self.get_random_location())
-        tap_direction = self.get_valid_tap_direction(tap_location)
-        self.set_board_position(tap_location, Starting(direction=tap_direction))
-
-
-    def get_valid_tap_direction(self, location, return_directions=False):
-        edges = [0, self.width-1, self.height-1]
-        directions = ["up", "right", "down", "left"]
-        x,y = location
-        if x == 0:
-            directions.pop(3)
-        if y == edges[2]:
-            directions.pop(2)
-        if x == edges[1]:
-            directions.pop(1)
-        if y == 0:
-            directions.pop(0)
-        if return_directions: return (random.choice(directions), directions)
-        return random.choice(directions)
-
-    def get_random_location(self, min_width=0, max_width=None, min_height=0, max_height=None):
-        """ Get a random location from within the specified range """
-        max_defaults = [self.width-1, self.height-1]
-        max_vars = [max_width, max_height]
-        max_width, max_height = [max_defaults[i] if max_vars[i] == None else max_vars[i] for i in range(2)]
-        return (random.randint(min_width, max_width), random.randint(min_height, max_height))
 
     def set_board_position(self, loc, obj):
         self.board[loc[1]*self.width + loc[0]] = obj
-
-    def print_board(self):
-        print("-"*130)
-        for i in range(self.height):
-            for j in range(self.width):
-                #print("| {:^10s} ".format(self.board[i*self.width + j].type), end="")
-                print("| {:^10s} ".format(ENCODE_ASCII[self.board[i*self.width + j].type]), end="")
-            print("|\n" + "-"*130)
 
 
 if __name__ == "__main__":
     env = PipeDreamEnv()
     state = env.reset()
-    env.get_random_location()
-    env.print_board()
-    print(state)
-
-    print(env.observation_space.shape)
+    env.render()
+    #print(state)
+    #print(env.observation_space.shape)
--- gym_pipedream/grid_elements.py
+++ gym_pipedream/grid_elements.py
@@ -1,19 +1,10 @@
-
+import random
 
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 7
 
 TILE_QUEUE_LEN = 5
 
-PLAYING_TILES = [
-    "vertical",
-    "horizontal",
-    "cross",
-    "leftup",
-    "leftdown",
-    "rightup",
-    "rightdown"
-]
 
 ENCODE_TILE = {
     "none":             0,
@@ -107,36 +98,108 @@ class HorizontalPipe(Pipe):
         self.transition["left"] = "right"
         self.transition["right"] = "left"
     
-class Cross(VerticalPipe, HorizontalPipe):
+class CrossPipe(VerticalPipe, HorizontalPipe):
     def __init__(self):
-        super(Cross, self).__init__(type="cross")
+        super(CrossPipe, self).__init__(type="cross")
 
-class LeftUp(Pipe):
+class LeftUpPipe(Pipe):
     def __init__(self):
         super().__init__("leftup")
         self.transition["left"] = "up"
         self.transition["up"] = "left"
 
-class LeftDown(Pipe):
+class LeftDownPipe(Pipe):
     def __init__(self):
         super().__init__("leftdown")
         self.transition["left"] = "down"
         self.transition["down"] = "left"
 
-class RightUp(Pipe):
+class RightUpPipe(Pipe):
     def __init__(self):
         super().__init__("rightup")
         self.transition["right"] = "up"
         self.transition["up"] = "right"
 
-class RightDown(Pipe):
+class RightDownPipe(Pipe):
     def __init__(self):
         super().__init__("rightdown")
         self.transition["right"] = "down"
         self.transition["down"] = "right"
 
-class Starting(Pipe):
+class StartingPipe(Pipe):
     def __init__(self, direction="down"):
         super().__init__("start"+direction)
         self.transition = direction
         self.state = 0.0001 # slight offset from 0 so it isnt empty
+
+PLAYING_TILES = [
+    VerticalPipe,
+    HorizontalPipe,
+    CrossPipe,
+    LeftUpPipe,
+    LeftDownPipe,
+    RightUpPipe,
+    RightDownPipe
+]
+
+class Board:
+    def __init__(self, width=BOARD_WIDTH, height=BOARD_HEIGHT):
+        self.width = width
+        self.height = height
+        self.tiles = [Floor()] * self.width * self.height
+
+    def get_tiles(self):
+        return self.tiles
+
+    def set_tile(self, location, object):
+        self.tiles[location[1]*self.width + location[0]] = object
+
+    def reset_board(self):
+        self.tiles = [Floor()] * self.width * self.height
+
+    def _calc_next_state(self):
+        raise NotImplementedError
+
+    def init_tap(self):
+        tap_location = list(self.get_random_location())
+        tap_direction = self.get_valid_tap_direction(tap_location)
+        self.set_tile(tap_location, StartingPipe(direction=tap_direction))
+
+
+    def get_valid_tap_direction(self, location, return_directions=False):
+        edges = [0, self.width-1, self.height-1]
+        directions = ["up", "right", "down", "left"]
+        x,y = location
+        if x == 0:
+            directions.pop(3)
+        if y == edges[2]:
+            directions.pop(2)
+        if x == edges[1]:
+            directions.pop(1)
+        if y == 0:
+            directions.pop(0)
+        if return_directions: return (random.choice(directions), directions)
+        return random.choice(directions)
+
+    def get_random_location(self, min_width=0, max_width=None, min_height=0, max_height=None):
+        """ Get a random location from within the specified range """
+        max_defaults = [self.width-1, self.height-1]
+        max_vars = [max_width, max_height]
+        max_width, max_height = [max_defaults[i] if max_vars[i] == None else max_vars[i] for i in range(2)]
+        return (random.randint(min_width, max_width), random.randint(min_height, max_height))
+
+    def __str__(self, print_width=130):
+        return_string = "-"*print_width + "\n"
+        for i in range(self.height):
+            for j in range(self.width):
+                return_string += "| {:^10s} ".format(self.tiles[i*self.width + j].type)
+            return_string += "|\n" + "-"*130 + "\n"
+
+        #print("-"*130)
+        #for i in range(self.height):
+        #    for j in range(self.width):
+        #        #print("| {:^10s} ".format(self.board[i*self.width + j].type), end="")
+        #        print("| {:^10s} ".format(ENCODE_ASCII[self.tiles[i*self.width + j].type]), end="")
+        #    print("|\n" + "-"*130)
+        return return_string
+    
