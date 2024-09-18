e3789d5f5970a0e03198a1cdda519083a0a5e717
akshilpatel <akshilpatel11@gmail.com>
2024-09-18 15:56:13 +0100

    Move images to within package and setup package to load in images when installed.

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(
     author="James Elson",
     url="https://github.com/Jelso13/gym-pipedream",
     packages=find_packages(),
+    package_data={
+        "gym_pipedream": ["images/**/*"],  # Include all files in images/ and its subfolders
+    },
 )
