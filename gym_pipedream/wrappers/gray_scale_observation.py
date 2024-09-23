1785ff6f2ede7e15e87cef74fc625a6d3a494beb
akshilpatel <akshilpatel11@gmail.com>
2024-09-23 13:15:02 +0100

    Added observation_space to gray scale wrapper

--- gym_pipedream/wrappers/gray_scale_observation.py
+++ gym_pipedream/wrappers/gray_scale_observation.py
@@ -1,10 +1,12 @@
 import gymnasium as gym
+import numpy as np
 from . import ImageObservation
 
 class GrayScaleObservation(gym.ObservationWrapper):
     def __init__(self, env):
         env = ImageObservation(env)
         super().__init__(env)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=env.observation_space.shape[:2], dtype=np.uint8)
 
     def observation(self, obs):
         import cv2
